a0e961f52dbca3a8cc4cbc246046b78656e30de5
MarchCraft <30194994+MarchCraft@users.noreply.github.com>
2024-07-12 17:43:37 +0200

    fix

--- mod/nixos/services/nextcloud.nix
+++ mod/nixos/services/nextcloud.nix
@@ -75,22 +75,21 @@
               '';
             };
 
-
-            system.stateVersion = "23.11";
-
-            networking = {
-              firewall = {
-                enable = true;
-                allowedTCPPorts = [ 80 ];
-              };
-              # Use systemd-resolved inside the container
-              # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-              useHostResolvConf = lib.mkForce false;
-            };
-
-            services.resolved.enable = true;
-
           };
+
+          system.stateVersion = "23.11";
+
+          networking = {
+            firewall = {
+              enable = true;
+              allowedTCPPorts = [ 80 ];
+            };
+            # Use systemd-resolved inside the container
+            # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+            useHostResolvConf = lib.mkForce false;
+          };
+
+          services.resolved.enable = true;
         };
       };
     };
