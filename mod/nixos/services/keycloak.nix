7882ab24849a76ba4e389fb06233558e09513e95
MarchCraft <30194994+MarchCraft@users.noreply.github.com>
2024-07-12 23:57:05 +0200

    fix: keycloak

--- mod/nixos/services/keycloak.nix
+++ mod/nixos/services/keycloak.nix
@@ -35,7 +35,7 @@
           services.keycloak = {
             enable = true;
             settings = {
-              hostname = "localhost";
+              hostname = "192.168.101.11";
               http-enabled = true;
               hostname-strict-https = false;
             };
