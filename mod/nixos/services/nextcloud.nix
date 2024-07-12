d53090ea21860b58c4715009a6d055f79ee9e704
MarchCraft <30194994+MarchCraft@users.noreply.github.com>
2024-07-12 17:40:16 +0200

    update: nextcloud postgres

--- mod/nixos/services/nextcloud.nix
+++ mod/nixos/services/nextcloud.nix
@@ -37,24 +37,60 @@
             enable = true;
             package = pkgs.nextcloud29;
             hostName = "localhost";
-            config.adminpassFile = config.sops.secrets.nextcloud_pass.path;
-          };
 
-          system.stateVersion = "23.11";
-
-          networking = {
-            firewall = {
-              enable = true;
-              allowedTCPPorts = [ 80 ];
+            phpExtraExtensions = all: [ all.pdlib all.bz2 all.smbclient ];
+            config = {
+              adminpassFile = config.sops.secrets.nextcloud_pass.path;
+              dbtype = "pgsql";
+              dbhost = "/var/run/postgresql";
+              dbuser = "postgres";
+              dbname = "nextcloud";
             };
-            # Use systemd-resolved inside the container
-            # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-            useHostResolvConf = lib.mkForce false;
+
+            phpOptions = {
+              "opcache.jit" = "1255";
+              "opcache.revalidate_freq" = "60";
+              "opcache.interned_strings_buffer" = "16";
+              "opcache.jit_buffer_size" = "128M";
+            };
+
+            configureRedis = true;
+            caching.apcu = true;
+            poolSettings = {
+              pm = "dynamic";
+              "pm.max_children" = "201";
+              "pm.max_requests" = "500";
+              "pm.max_spare_servers" = "150";
+              "pm.min_spare_servers" = "50";
+              "pm.start_servers" = "50";
+            };
+
+            services.postgresql = {
+              enable = true;
+              ensureDatabases = [ "nextcloud" ];
+              package = pkgs.postgresql_16_jit;
+              authentication = pkgs.lib.mkOverride 10 ''
+                #type database  DBuser  auth-method
+                local all       all     trust
+              '';
+            };
+
+
+            system.stateVersion = "23.11";
+
+            networking = {
+              firewall = {
+                enable = true;
+                allowedTCPPorts = [ 80 ];
+              };
+              # Use systemd-resolved inside the container
+              # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+              useHostResolvConf = lib.mkForce false;
+            };
+
+            services.resolved.enable = true;
+
           };
-
-          services.resolved.enable = true;
-
         };
       };
-    };
-}
+    }
