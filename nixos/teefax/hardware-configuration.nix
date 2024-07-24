0dba563174be62968ac362fa98a1054302a79db4
Florian Schubert <florian.schubert.sg@gmail.com>
2024-07-24 14:55:42 +0200

    feat: added impermanence

--- nixos/teefax/hardware-configuration.nix
+++ nixos/teefax/hardware-configuration.nix
@@ -21,6 +21,12 @@
       options = [ "subvol=root" ];
     };
 
+  fileSystems."/persist" =
+    {
+      device = "/dev/disk/by-uuid/bf6dacbc-1e3a-4052-ae31-ac58e95d7bad";
+      fsType = "btrfs";
+      options = [ "subvol=persist" ];
+    };
   fileSystems."/nix" =
     {
       device = "/dev/disk/by-uuid/bf6dacbc-1e3a-4052-ae31-ac58e95d7bad";
@@ -34,6 +40,31 @@
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
+
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/disk/by-uuid/bf6dacbc-1e3a-4052-ae31-ac58e95d7bad/root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
   swapDevices =
     [{ device = "/dev/disk/by-uuid/8593d304-8d52-49c3-a5cb-267be6beb773"; }];
