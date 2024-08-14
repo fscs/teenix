46405e6f8bd9f5660d6af2d3f3bf70043f209916
Felix Nilles <felix@dienilles.de>
2024-08-14 14:03:24 +0200

    fix: passbolt

--- mod/nixos/services/passbolt/container.nix
+++ mod/nixos/services/passbolt/container.nix
@@ -13,30 +13,36 @@ in
   virtualisation.arion = {
     backend = "docker";
     projects = {
-      "passbolt".settings.services."passbolt".service = {
-        image = "passbolt/passbolt";
-        restart = "unless-stopped";
-        env_file = [ host-config.sops.secrets.passbolt.path ];
-        ports = [ "8080:8080" ];
-        environment = {
-          DATASOURCES_DEFAULT_HOST = "mariadb";
-          DATASOURCES_DEFAULT_USERNAME = "passbolt";
-          DATASOURCES_DEFAULT_DATABASE = "passbolt";
-          DATASOURCES_DEFAULT_PORT = 3306;
-          DATASOURCES_QUOTE_IDENTIFIER = "true";
-          APP_FULL_BASE_URL = "https://passbolt.fscs-hhu.de";
-          EMAIL_DEFAULT_FROM = "fscs@hhu.de";
-          EMAIL_TRANSPORT_DEFAULT_TLS = "true";
-          PASSBOLT_KEY_EMAIL = "fscs@hhu.de";
+      "passbolt".settings = {
+        host.uid = 1000;
+        services."passbolt".service = {
+          image = "passbolt/passbolt";
+          restart = "unless-stopped";
+          env_file = [ host-config.sops.secrets.passbolt.path ];
+          ports = [ "8080:8080" ];
+          environment = {
+            DATASOURCES_DEFAULT_HOST = "mariadb";
+            DATASOURCES_DEFAULT_USERNAME = "passbolt";
+            DATASOURCES_DEFAULT_DATABASE = "passbolt";
+            DATASOURCES_DEFAULT_PORT = 3306;
+            DATASOURCES_QUOTE_IDENTIFIER = "true";
+            APP_FULL_BASE_URL = "https://passbolt.fscs-hhu.de";
+            EMAIL_DEFAULT_FROM = "fscs@hhu.de";
+            EMAIL_TRANSPORT_DEFAULT_TLS = "true";
+            PASSBOLT_KEY_EMAIL = "fscs@hhu.de";
+          };
         };
       };
-      "mariadb".settings.services."mariadb".service = {
-        image = "mariadb";
-        restart = "unless-stopped";
-        env_file = [ host-config.sops.secrets.passbolt_mariadb.path ];
-        environment = {
-          MYSQL_DATABASE = "passbolt";
-          MYSQL_USER = "passbolt";
+      "mariadb".settings = {
+        host.uid = 1000;
+        services."mariadb".service = {
+          image = "mariadb";
+          restart = "unless-stopped";
+          env_file = [ host-config.sops.secrets.passbolt_mariadb.path ];
+          environment = {
+            MYSQL_DATABASE = "passbolt";
+            MYSQL_USER = "passbolt";
+          };
         };
       };
     };
