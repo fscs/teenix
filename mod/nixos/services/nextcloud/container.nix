8bc5610641333bb4c34df1a05a1a2a8c7fe5cfb9
Florian Schubert <florian.schubert.sg@gmail.com>
2024-10-03 01:31:43 +0200

    notify push: nextcloud is now localhost

--- mod/nixos/services/nextcloud/container.nix
+++ mod/nixos/services/nextcloud/container.nix
@@ -20,7 +20,6 @@ in
     package = pkgs-master.nextcloud30;
     notify_push = {
       enable = true;
-      bendDomainToLocalhost = true;
       # The module checks in a weird way if we use a unix socket
       dbhost = "/run/mysqld/mysqld.sock";
       dbuser = "nextcloud@localhost:";
