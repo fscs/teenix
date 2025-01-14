72dfa5d609e6758416667d831173e0d7baae35ad
Felix Nilles <felix@dienilles.de>
2025-01-14 14:09:32 +0100

    gotta go fast

--- mod/nixos/services/nextcloud/container.nix
+++ mod/nixos/services/nextcloud/container.nix
@@ -1,10 +1,9 @@
-{
-  lib,
-  config,
-  host-config,
-  pkgs,
-  pkgs-master,
-  ...
+{ lib
+, config
+, host-config
+, pkgs
+, pkgs-master
+, ...
 }:
 let
   opts = host-config.teenix.services.nextcloud;
@@ -74,6 +73,27 @@ in
       "pm.min_spare_servers" = "50";
       "pm.start_servers" = "50";
     };
+    services.nginx.virtualHosts."nextcloud.inphima.de".extraConfig = lib.mkForce ''
+      index index.php index.html /index.php$request_uri;
+      add_header X-XSS-Protection "1; mode=block" always;
+      add_header X-Robots-Tag "noindex, nofollow" always;
+      add_header X-Download-Options noopen always;
+      add_header X-Permitted-Cross-Domain-Policies none always;
+      add_header X-Frame-Options sameorigin always;
+      add_header X-Content-Type-Options nosniff;
+      add_header Referrer-Policy no-referrer always;
+      add_header Strict-Transport-Security "max-age=${toString config.services.nextcloud.nginx.hstsMaxAge}; includeSubDomains" always;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      client_max_body_size ${config.services.nextcloud.maxUploadSize};
+      fastcgi_buffers 64 4K;
+      fastcgi_hide_header X-Powered-By;
+      gzip on;
+      gzip_vary on;
+      gzip_comp_level 4;
+      gzip_min_length 256;
+      gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+      gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy text/javascript;
+    '';
   };
 
   networking = {
