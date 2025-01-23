d34588ee19317000b4e170edc8328777b5c906bb
Felix Nilles <felix@dienilles.de>
2025-01-23 17:55:40 +0100

    loki

--- mod/nixos/services/prometheus/container.nix
+++ mod/nixos/services/prometheus/container.nix
@@ -69,6 +69,22 @@ in
     ];
   };
 
+  services.loki = {
+    enable = true;
+    configFile = ./loki-local-config.yaml;
+  };
+
+  systemd.services.promtail = {
+    description = "Promtail service for Loki";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.grafana-loki}/bin/promtail --config.file ${./promtail.yaml}
+      '';
+    };
+  };
+
   services.grafana = {
     package = pkgs-stable.grafana;
     enable = true;
