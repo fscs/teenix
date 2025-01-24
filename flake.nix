872b48f18d2e32feb4c79e9d686ea6a86f42346a
Felix Nilles <felix@dienilles.de>
2025-01-24 12:49:00 +0100

    kp bro diggah

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # NOTE: change channel in gitlab runner when updating this
     nixpkgs-master.url = "github:nixos/nixpkgs";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     flake-programs-sqlite = {
       url = "github:wamserma/flake-programs-sqlite";
