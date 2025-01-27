2bc84ca66d34ed2352ec95ae702790aecb75d0c3
Felix Nilles <felix@dienilles.de>
2025-01-27 20:35:57 +0100

    fix alloy

--- mod/nixos/services/alloy.nix
+++ mod/nixos/services/alloy.nix
@@ -1,40 +1,43 @@
-{
-  lib,
-  config,
-  ...
+{ lib
+, config
+, ...
 }: {
-  options.teenix.services.alloy = let
-    t = lib.types;
-  in {
-    enable = lib.mkEnableOption "enable grafana alloy";
-    loki = {
-      exporterUrl = lib.mkOption {
-        description = "loki url to export to";
-        type = t.nonEmptyStr;
+  options.teenix.services.alloy =
+    let
+      t = lib.types;
+    in
+    {
+      enable = lib.mkEnableOption "enable grafana alloy";
+      loki = {
+        exporterUrl = lib.mkOption {
+          description = "loki url to export to";
+          type = t.nonEmptyStr;
+        };
+        exporterName = lib.mkOption {
+          type = t.nonEmptyStr;
+          default = "loki_write";
+        };
       };
-      exporterName = lib.mkOption {
-        type = t.nonEmptyStr;
-        default = "loki_write";
+      extraConfig = lib.mkOption {
+        type = t.lines;
       };
     };
-    extraConfig = lib.mkOption {
-      type = t.lines; 
+
+  config =
+    let
+      cfg = config.teenix.services.alloy;
+    in
+    lib.mkIf cfg.enable {
+      services.alloy.enable = true;
+
+      teenix.services.alloy.extraConfig = lib.mkAfter ''
+        loki.write "${cfg.loki.exporterName}" {
+          endpoint {
+            url ="http://${cfg.loki.exporterUrl}/loki/api/v1/push"
+          }
+        } 
+      '';
+
+      environment.etc."alloy/config.alloy".text = cfg.extraConfig;
     };
-  };
-
-  config = let
-    cfg = config.teenix.services.alloy;
-  in lib.mkIf cfg.enable {
-    services.alloy.enable = true;
-
-    teenix.services.alloy.extraConfig = lib.mkAfter ''
-      loki.write "${cfg.loki.exporterName}" {
-        endpoint {
-          url ="http://${cfg.loki.exporterUrl}/loki/api/v1/push"
-        }
-      } 
-    '';
-
-    environment.etc."alloy/config.alloy".text = cfg.extraConfig;
-  };
 }
