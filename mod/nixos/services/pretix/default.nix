82da26586b810e6455b729bef9236354a1777b47
0x5a4 <bej86nug@hhu.de>
2025-01-29 15:33:31 +0100

    migrate pretix

--- mod/nixos/services/pretix/default.nix
+++ mod/nixos/services/pretix/default.nix
@@ -1,8 +1,6 @@
 {
   lib,
   config,
-  pkgs,
-  inputs,
   ...
 }:
 {
@@ -19,45 +17,35 @@
       opts = config.teenix.services.pretix;
     in
     lib.mkIf opts.enable {
-      teenix.services.traefik.services."pretix" = {
+      teenix.services.traefik.services.pretix = {
         router.rule = "Host(`${opts.hostname}`)";
         servers = [ "http://${config.containers.pretix.localAddress}" ];
-        healthCheck = {
-          path = "/control";
-        };
       };
 
-      nix-tun.storage.persist.subvolumes."pretix".directories = {
-        "/postgres" = {
-          owner = "${builtins.toString config.containers.pretix.config.users.users.postgres.uid}";
-          mode = "0700";
-        };
-      };
-
-      containers.pretix = {
-        ephemeral = true;
-        autoStart = true;
-        privateNetwork = true;
-        hostAddress = "192.168.108.10";
-        localAddress = "192.168.108.11";
-        bindMounts = {
-          "db" = {
-            hostPath = "${config.nix-tun.storage.persist.path}/pretix/postgres";
-            mountPoint = "/var/lib/postgresql";
-            isReadOnly = false;
+      teenix.containers.pretix = {
+        config = {
+          services.pretix = {
+            enable = true;
+            database.createLocally = true;
+            nginx.domain = opts.hostname;
+            settings = {
+              mail.from = "${opts.email}";
+              pretix = {
+                instance_name = "${opts.hostname}";
+                url = "https://${opts.hostname}";
+              };
+            };
           };
-          "resolv" = {
-            hostPath = "/etc/resolv.conf";
-            mountPoint = "/etc/resolv.conf";
-          };
+
+          system.stateVersion = "23.11";
         };
 
-        specialArgs = {
-          inherit inputs;
-          host-config = config;
+        networking = {
+          useResolvConf = true;
+          ports.tcp = [ 80 ];
         };
 
-        config = import ./container.nix;
+        mounts.postgres.enable = true;
       };
     };
 }
