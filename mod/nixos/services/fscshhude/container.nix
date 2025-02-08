370eb1caf7df77de9679e4404eb58fb156ea8b06
0x5a4 <bej86nug@hhu.de>
2025-02-08 13:26:56 +0100

    update website

--- mod/nixos/services/fscshhude/container.nix
+++ mod/nixos/services/fscshhude/container.nix
@@ -18,12 +18,19 @@
     package = inputs.fscshhude.inputs.server.packages.${pkgs.stdenv.system}.default;
     content = config.services.fscs-website-setup.dataDir;
     environmentFile = host-config.sops.secrets.fscshhude-env.path;
+
     authUrl = "https://${host-config.teenix.services.authentik.hostname}/application/o/authorize/";
     tokenUrl = "https://${host-config.teenix.services.authentik.hostname}/application/o/token/";
     userInfoUrl = "https://${host-config.teenix.services.authentik.hostname}/application/o/userinfo/";
+
     allowedCorsOrigins = [
-      "https://${host-config.teenix.services.sitzungsverwaltung.hostname}" 
+      "https://${host-config.teenix.services.sitzungsverwaltung.hostname}"
     ];
+
+    calendars = {
+      events = "https://nextcloud.inphima.de/remote.php/dav/public-calendars/CAx5MEp7cGrQ6cEe?export";
+      branchen = "https://nextcloud.inphima.de/remote.php/dav/public-calendars/CKpykNdtKHkA6Z9B?export";
+    };
   };
 
   system.stateVersion = "24.11";
