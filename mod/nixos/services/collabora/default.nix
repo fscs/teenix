4ba323b2caee129f7d9b608348c4d13c5bb249bb
0x5a4 <1444@wienstroer.net>
2025-04-30 22:14:12 +0200

    Revert "update colmena"
    This reverts commit 7deb5dbfdbd1236276ef1432bb225108552689b7.

--- mod/nixos/services/collabora/default.nix
+++ mod/nixos/services/collabora/default.nix
@@ -32,6 +32,7 @@
           systemd.services.coolwsd.environment.server_name = config.teenix.services.collabora.hostname;
           services.collabora-online = {
             enable = true;
+            package = pkgs-stable.collabora-online;
             aliasGroups = lib.singleton {
               host = "https://${config.teenix.services.collabora.nextcloudHost}:443";
             };
