12276a0b8db1bb6cb4224b218e119421b6d28625
0x5a4 <1444@wienstroer.net>
2025-05-13 11:34:08 +0200

    authentik background

--- mod/nixos/services/authentik/container.nix
+++ mod/nixos/services/authentik/container.nix
@@ -1,8 +1,30 @@
 {
   inputs,
   host-config,
+  pkgs,
   ...
 }:
+let
+  background = pkgs.fetchurl {
+    url = "https://nextcloud.phynix-hhu.de/s/Mf3Y6JqgNmxx9Wo/download";
+    hash = "sha256-PKUIuEeW2IIeuYvYcOP/8QEmLNMIX986b23ria1CgTg=";
+  };
+
+  authentikComponents =
+    ((inputs.authentik-nix.lib.mkAuthentikScope { inherit pkgs; }).overrideScope (
+      final: prev: {
+        authentikComponents = prev.authentikComponents // {
+          frontend = prev.authentikComponents.frontend.overrideAttrs (oA: {
+            installPhase =
+              oA.installPhase
+              + ''
+                cp ${background} $out/dist/assets/images/flow_background.jpg
+              '';
+          });
+        };
+      }
+    )).authentikComponents;
+in
 {
   imports = [
     inputs.authentik-nix.nixosModules.default
@@ -12,6 +34,8 @@
     enable = true;
     environmentFile = host-config.sops.templates.authentik.path;
     createDatabase = true;
+
+    inherit authentikComponents;
 
     settings = {
       email = {
