95e3370da68755c320d1181df1fcbfe321dc4f11
Felix Nilles <felix@dienilles.de>
2025-05-13 17:06:25 +0200

    sebigbos impermanence

--- nixos/sebigbos/hardware-configuration.nix
+++ nixos/sebigbos/hardware-configuration.nix
@@ -27,30 +27,30 @@
 
   fileSystems."/persist".neededForBoot = true;
 
-  # boot.initrd.postDeviceCommands = lib.mkAfter ''
-  #   mkdir /btrfs_tmp
-  #   mount /dev/disk/by-uuid/f312acc1-a543-4513-a487-ca7d8f302a8b /btrfs_tmp
-  #   if [[ -e /btrfs_tmp/root ]]; then
-  #       mkdir -p /btrfs_tmp/old_roots
-  #       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #       mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #   fi
-  #
-  #   delete_subvolume_recursively() {
-  #       IFS=$'\n'
-  #       for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #           delete_subvolume_recursively "/btrfs_tmp/$i"
-  #       done
-  #       btrfs subvolume delete "$1"
-  #   }
-  #
-  #   for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-  #       delete_subvolume_recursively "$i"
-  #   done
-  #
-  #   btrfs subvolume create /btrfs_tmp/root
-  #   umount /btrfs_tmp
-  # '';
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/disk/by-uuid/3b21de9a-9b9e-4b44-9fa8-f1716b0ce78f /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
   networking.useDHCP = lib.mkDefault true;
 
