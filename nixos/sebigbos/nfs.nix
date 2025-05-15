ce95e5edadfd653634309a891dcbece1fb8d9a1a
Felix Nilles <felix@dienilles.de>
2025-05-15 19:18:21 +0200

    nfs share

--- nixos/sebigbos/nfs.nix
+++ nixos/sebigbos/nfs.nix
@@ -1,7 +1,7 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /mnt/share         134.99.147.42(rw,root_squash,sync) 134.99.147.43(rw,root_squash,sync)
+    /mnt/share         134.99.147.42(rw,sync,no_root_squash) 134.99.147.43(rw,sync,no_root_squash)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
